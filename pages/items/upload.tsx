b6e50aa299f5e70d6537ba2f24f9fd52ecc14226
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-02-06 08:59:41 +0900

    #5.7 Community Detail

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="py-16 px-4">
+    <div className="py-10 px-4">
       <div>
         <label className="flex h-48 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-gray-600 hover:border-orange-500 hover:text-orange-500">
           <svg
