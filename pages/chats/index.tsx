02c2d2711ad72098702ee800aaca1948e38bd974
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-02-06 14:58:28 +0900

    #5.17 Layout

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="divide-y py-10 ">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex cursor-pointer items-center space-x-3 px-4 py-3"
-        >
-          <div className="h-12 w-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm  text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px] ">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="flex cursor-pointer items-center space-x-3 px-4 py-3"
+          >
+            <div className="h-12 w-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm  text-gray-500">
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
