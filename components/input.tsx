9084699a3e68c4169bfd8ddad7871a65d4c6f0f9
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-02-21 22:55:20 +0900

    #8 REFACTORING; Enter Form

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,21 @@
+import type { UseFormRegisterReturn } from "react-hook-form";
+
 interface InputProps {
   label: string;
   name: string;
   kind?: "text" | "phone" | "price";
-  [key: string]: any;
+  register: UseFormRegisterReturn;
+  type: string;
+  required: boolean;
 }
 
 export default function Input({
   label,
   name,
   kind = "text",
-  ...rest
+  register,
+  type,
+  required,
 }: InputProps) {
   return (
     <div>
@@ -23,7 +29,9 @@ export default function Input({
         <div className="relative flex items-center  rounded-md shadow-sm">
           <input
             id={name}
-            {...rest}
+            required={required}
+            {...register}
+            type={type}
             className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
         </div>
@@ -35,7 +43,9 @@ export default function Input({
           </div>
           <input
             id={name}
-            {...rest}
+            required={required}
+            {...register}
+            type={type}
             className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
           <div className="pointer-events-none absolute right-0 flex items-center pr-3">
@@ -50,7 +60,9 @@ export default function Input({
           </span>
           <input
             id={name}
-            {...rest}
+            required={required}
+            {...register}
+            type={type}
             className="w-full appearance-none rounded-md rounded-l-none border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
         </div>
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
 import { cls } from "../libs/utils";
 
+interface EnterForm {
+  email?: string;
+  phone?: string;
+}
+
 const Enter = () => {
+  const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+  const onEmailClick = () => {
+    reset(), setMethod("email");
+  };
+  const onPhoneClick = () => {
+    reset(), setMethod("phone");
+  };
+  const onValid = (data: EnterForm) => {
+    console.log(data);
+  };
   return (
     <div className="mt-16  px-4">
       <h3 className="text-center text-3xl font-bold">Enter to Carrot</h3>
@@ -38,12 +52,22 @@ const Enter = () => {
             </button>
           </div>
         </div>
-        <form className="mt-8 flex flex-col space-y-4">
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="mt-8 flex flex-col space-y-4"
+        >
           {method === "email" ? (
-            <Input name="email" label="Email address" type="email" required />
+            <Input
+              register={register("email", { required: true })}
+              name="email"
+              label="Email address"
+              type="email"
+              required
+            />
           ) : null}
           {method === "phone" ? (
             <Input
+              register={register("phone", { required: true })}
               name="phone"
               label="Phone number"
               type="number"
