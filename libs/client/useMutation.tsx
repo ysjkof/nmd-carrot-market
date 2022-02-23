3278234fc88337f51621c58bc101d8dcee01c07b
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-02-23 21:21:35 +0900

    shorten state in useMutation

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -8,20 +8,22 @@ interface UseMutationState {
 type useMutationResult = [(data: any) => void, UseMutationState];
 
 export default function useMutation(url: string): useMutationResult {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<undefined | any>(undefined);
-  const [error, setError] = useState<undefined | any>(undefined);
+  const [state, setState] = useState<UseMutationState>({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
   function mutation(data: any) {
-    setLoading(true);
+    setState(() => ({ loading: true }));
     fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {}))
-      .then(setData)
-      .catch(setError)
-      .finally(() => setLoading(false));
+      .then(setState)
+      .catch(setState)
+      .finally(() => setState(() => ({ loading: false })));
   }
-  return [mutation, { loading, data, error }];
+  return [mutation, state];
 }
