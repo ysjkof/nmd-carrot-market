17d253e8db74c2d453f41a7120e7967ccafbb805
isj <ysjkof@gmail.com>
2022-02-25 10:19:37 +0900

    #9.5 Sending Email; with SendGrid

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,5 +1,6 @@
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import mail from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
 
@@ -7,6 +8,7 @@ import twilio from "twilio";
 // 여기서 실행할 핵심 기능은 handler다
 // 에러처리 등 반복되는 기능을 쉽게쓰려고 withHandler를 사용한다.
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 // 여기서 async 지워도 되는 거 같은데 일단 보류.
@@ -38,10 +40,21 @@ async function handler(
   if (phone) {
     const message = await twilioClient.messages.create({
       messagingServiceSid: process.env.TWILIO_SERVICE_SID,
+      // to는 body에서 받은 번호를 넣어야 한다. 테스트용이라 내 번호로 보냄.
       to: process.env.MY_PHONE!,
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (mail) {
+    const email = await mail.send({
+      from: "ysjkof@gmail.com",
+      //  to는 body에서 받은 이메일을 넣어야 한다. 테스트용이라 내 이메일로 보냄.
+      to: "ysjkof@gmail.com",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
   console.log(token);
   // token에 connect를 쓸 때
