280de34fe1ea1770cd18aa25340072270f8846c9
isj <ysjkof@gmail.com>
2022-03-03 11:48:00 +0900

    #10.4 SWR, useUser Refactor

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,19 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+// fetcher 함수는 데이터를 불러와서 리턴한다
+// const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me");
+  //   _app.tsx에서 SWRConfig에 fetcher를 설정했기 때문에 페쳐를 입력하지 않아도 된다.
+  //   const { data, error } = useSWR("/api/users/me", fetcher);
   const router = useRouter();
   useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+  return { user: data?.profile, isLoading: !data && !error };
 }
