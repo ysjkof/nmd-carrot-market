3a6ff5759fb8c2ef11759a389b44174e822dd3c6
isj <ysjkof@gmail.com>
2022-03-03 12:40:46 +0900

    #11.1 Upload Form

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,10 +1,20 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface TextAreaProps {
   label?: string;
   name?: string;
+  register: UseFormRegisterReturn;
+  required: boolean;
   [key: string]: any;
 }
 
-export default function TextArea({ label, name, ...rest }: TextAreaProps) {
+export default function TextArea({
+  label,
+  name,
+  register,
+  required,
+  ...rest
+}: TextAreaProps) {
   return (
     <div>
       {label ? (
@@ -17,6 +27,8 @@ export default function TextArea({ label, name, ...rest }: TextAreaProps) {
       ) : null}
       <textarea
         id={name}
+        {...register}
+        required={required}
         className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 "
         rows={4}
         {...rest}
--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -3,11 +3,26 @@ import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+
+interface UploadProductForm {
+  name: string;
+  price: number;
+  description: string;
+}
 
 const Upload: NextPage = () => {
+  const { register, handleSubmit } = useForm<UploadProductForm>();
+  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+  const onValid = (data: UploadProductForm) => {
+    if (loading) return;
+    uploadProduct(data);
+  };
+
   return (
     <Layout canGoBack title="Upload Product">
-      <form className="space-y-4 p-4">
+      <form className="space-y-4 p-4" onSubmit={handleSubmit(onValid)}>
         <div>
           <label className="flex h-48 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-gray-600 hover:border-orange-500 hover:text-orange-500">
             <svg
@@ -27,10 +42,28 @@ const Upload: NextPage = () => {
             <input className="hidden" type="file" />
           </label>
         </div>
-        <Input required label="Name" name="name" type="text" />
-        <Input required label="Price" name="price" type="text" kind="price" />
-        <TextArea name="description" label="Description" />
-        <Button text="Upload item" />
+        <Input
+          register={register("name", { required: true })}
+          required
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <Input
+          register={register("price", { required: true })}
+          required
+          label="Price"
+          name="price"
+          type="text"
+          kind="price"
+        />
+        <TextArea
+          register={register("description", { required: true })}
+          required
+          name="description"
+          label="Description"
+        />
+        <Button text={loading ? "Loading..." : "Upload item"} />
       </form>
     </Layout>
   );
