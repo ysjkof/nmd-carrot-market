8ad11de665de9a32acb9b49d5e3e52a9ee9533b5
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-03-13 23:05:16 +0900

    #11.7 Favorite Products part Two

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
   const product = await client.product.findUnique({
     where: {
       id: +id.toString(),
@@ -33,7 +36,16 @@ async function handler(
       AND: { id: { not: product?.id } },
     },
   });
-  res.json({ ok: true, product, relatedProducts });
+  const isLiked = Boolean(
+    await client.fav.findFirst({
+      where: {
+        productId: product?.id,
+        userId: user?.id,
+      },
+      select: { id: true },
+    })
+  );
+  res.json({ ok: true, product, isLiked, relatedProducts });
 }
 
 export default withApiSession(
