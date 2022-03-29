b85211fe281bb0aabcb491eee553d07c8d5b7a81
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-03-29 15:05:28 +0900

    #12.2 Post Detail

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,59 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      // id가 배열이 될 수도 있어 toString()하고 숫자로 바꿔준다.
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+  if (!post) {
+    return res.status(404).json({
+      ok: false,
+      error: "POST를 찾을 수 없습니다.",
+    });
+  }
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
